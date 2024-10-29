6f02bd3013b2e2deff51aa35f37474a24488a7aa
RainelC <rainelmartinez13@gmail.com>
2024-10-29 03:44:46 -0400

    InitialCommit

---
+++ InternetBanking.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using InternetBanking.Core.Application.Interfaces.Repositories;
+using InternetBanking.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Infrastructure.Persistence.Repositories
+{
+    internal class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : class
+    {
+        private readonly AppDbContext _dbContext;
+
+        public GenericRepository(AppDbContext context)
+        {
+
+            _dbContext = context;
+        }
+        public virtual async Task AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(Entity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<List<Entity>> GetAllAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public virtual async Task<Entity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+    }
+}
---
+++ InternetBanking.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,39 @@
+﻿using InternetBanking.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace InternetBanking.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration config)
+        {
+
+            if (config.GetValue<bool>("UseDatabaseinMemory"))
+            {
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                services.AddDbContext<AppDbContext>(opt =>
+                                    opt.UseInMemoryDatabase("AppDb"));
+            }
+            else
+            {
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                services.AddDbContext<AppDbContext>(opt =>
+                                    opt.UseSqlServer(connectionString,
+                                    m => m.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+                                    ));
+            }
+
+
+            //Repositories
+
+
+
+            //Servicies
+
+
+        }
+    }
+}
